55ca2efa4c8e11240cd442949362e82c5051abbc
Martin Peshevski <martin.m.pesevski@gmail.com>
2017-03-22 19:02:20 +0100

    initial commit

---
+++ MemeGenerator/MemeGenerator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MemeGenerator
+//
+//  Created by Martin Peshevski on 3/22/17.
+//  Copyright © 2017 Martin Peshevski. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ MemeGenerator/MemeGenerator/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  MemeGenerator
+//
+//  Created by Martin Peshevski on 3/22/17.
+//  Copyright © 2017 Martin Peshevski. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
