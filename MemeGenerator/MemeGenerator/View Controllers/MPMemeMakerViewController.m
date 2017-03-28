a6bb2c918bdf0afa1746fd13fbb8c9b4993a5826
Martin Peshevski <martin.m.pesevski@gmail.com>
2017-03-28 08:34:22 +0200

    adding text to image implemented

--- MemeGenerator/MemeGenerator/View Controllers/MPMemeMakerViewController.m
+++ MemeGenerator/MemeGenerator/View Controllers/MPMemeMakerViewController.m
@@ -9,6 +9,7 @@
 #import "MPMemeMakerViewController.h"
 #import "Masonry.h"
 #import "Constants.h"
+#import "MPColorManager.h"
 
 #define kMemeImageHeightWidth 300
 
@@ -22,6 +23,8 @@
 
 @property (nonatomic, strong) UITextField *topTextField;
 @property (nonatomic, strong) UITextField *bottomTextField;
+
+@property (nonatomic, strong) UIButton *createButton;
 
 @end
 
@@ -60,6 +63,7 @@
     self.topLabel.numberOfLines = 0;
     self.topLabel.lineBreakMode = NSLineBreakByWordWrapping;
     self.topLabel.adjustsFontSizeToFitWidth = YES;
+    self.topLabel.font = [UIFont fontWithName:@"impact" size:20];
     
     self.bottomLabel = [[UILabel alloc] init];
     self.bottomLabel.text = @"BOTTOM TEXT";
@@ -68,7 +72,8 @@
     self.bottomLabel.numberOfLines = 0;
     self.bottomLabel.lineBreakMode = NSLineBreakByWordWrapping;
     self.bottomLabel.adjustsFontSizeToFitWidth = YES;
-    
+    self.bottomLabel.font = [UIFont fontWithName:@"impact" size:20];
+
     self.topTextField = [[UITextField alloc] init];
     self.topTextField.delegate = self;
     self.topTextField.backgroundColor = [UIColor whiteColor];
@@ -80,12 +85,19 @@
     self.bottomTextField.backgroundColor = [UIColor whiteColor];
     self.bottomTextField.autocorrectionType = UITextAutocorrectionTypeNo;
     self.bottomTextField.placeholder = @"Enter bottom text here";
+    
+    self.createButton = [[UIButton alloc] init];
+    [self.createButton setTitle:@"Create" forState:UIControlStateNormal];
+    [self.createButton addTarget:self action:@selector(onCreate) forControlEvents:UIControlEventTouchUpInside];
+    [self.createButton setTitleColor:[MPColorManager getLabelColorWhite] forState:UIControlStateNormal];
+    self.createButton.backgroundColor = [MPColorManager getNavigationBarColor];
 
     [self.view addSubview:self.memeImageView];
     [self.view addSubview:self.topLabel];
     [self.view addSubview:self.bottomLabel];
     [self.view addSubview:self.topTextField];
     [self.view addSubview:self.bottomTextField];
+    [self.view addSubview:self.createButton];
 }
 
 - (void)setConstraints
@@ -119,6 +131,12 @@
         make.top.equalTo(self.topTextField.mas_bottom).offset(kLeftRightPadding);
         make.height.equalTo(@(kMemeTextfieldHeight));
     }];
+    [self.createButton mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.centerX.equalTo(self.view);
+        make.height.equalTo(@(kLoginButtonHeight));
+        make.width.equalTo(@(kScreenWidth /3));
+        make.bottom.equalTo(self.view).offset(-kLeftRightPadding);
+    }];
 }
 
 #pragma mark - textfield methods
@@ -132,6 +150,53 @@
         self.bottomLabel.text = [newString uppercaseString];
     }
     return YES;
+}
+
+#pragma mark - button actions
+
+- (void)onCreate
+{
+    UIFont *font = [UIFont fontWithName:@"impact" size:20];
+    NSDictionary *attributes = @{NSFontAttributeName: font};
+    float labelWidth = (self.memeImageView.frame.size.width - 2 * kLeftRightPadding);
+    CGRect topLabelRect = [self.topLabel.text boundingRectWithSize:CGSizeMake(labelWidth, CGFLOAT_MAX)
+                                                           options:NSStringDrawingUsesLineFragmentOrigin
+                                                        attributes:attributes
+                                                           context:nil];
+    
+    CGRect bottomLabelRect = [self.bottomLabel.text boundingRectWithSize:CGSizeMake(labelWidth, CGFLOAT_MAX)
+                                                           options:NSStringDrawingUsesLineFragmentOrigin
+                                                        attributes:attributes
+                                                           context:nil];
+    
+    UIGraphicsBeginImageContext(self.memeImageView.frame.size);
+    [self.memeImage drawInRect:CGRectMake(0,0,self.memeImageView.frame.size.width,self.memeImageView.frame.size.height)];
+    float pointx = self.memeImageView.frame.size.width/2 - topLabelRect.size.width/2;
+    CGRect rect = CGRectMake(pointx, kLeftRightPadding, topLabelRect.size.width, topLabelRect.size.height);
+    [[UIColor whiteColor] set];
+    
+    NSMutableParagraphStyle *paragraphStyle = [[NSParagraphStyle defaultParagraphStyle] mutableCopy];
+    paragraphStyle.lineBreakMode = NSLineBreakByWordWrapping;
+    paragraphStyle.alignment = NSTextAlignmentCenter;
+
+    [self.topLabel.text drawInRect:CGRectIntegral(rect)
+                        withAttributes:@{NSFontAttributeName:font,
+                                         NSParagraphStyleAttributeName: paragraphStyle,
+                                         NSForegroundColorAttributeName: [MPColorManager getLabelColorWhite]}];
+
+    pointx = self.memeImageView.frame.size.width/2 - bottomLabelRect.size.width/2;
+    rect = CGRectMake(pointx,
+                      self.memeImageView.frame.size.height - (bottomLabelRect.size.height +kLeftRightPadding),
+                      bottomLabelRect.size.width,
+                      bottomLabelRect.size.height);
+
+    [self.bottomLabel.text drawInRect:CGRectIntegral(rect) withAttributes:@{NSFontAttributeName:font,
+                                                                                NSParagraphStyleAttributeName: paragraphStyle,
+                                                                                NSForegroundColorAttributeName: [MPColorManager getLabelColorWhite]}];
+    UIImage *newImage = UIGraphicsGetImageFromCurrentImageContext();
+    UIGraphicsEndImageContext();
+    
+    UIImageWriteToSavedPhotosAlbum(newImage, nil, nil, nil);
 }
 
 @end
